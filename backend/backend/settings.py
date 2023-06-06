ac27dce5473be9cc2555b7c7f815634f5f0abb53
Alexandr Batanov <pokertrend@yandex.ru>
2023-06-06 14:45:13 +0400

    добавлена пагинация

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,8 +110,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 6
+    'DEFAULT_PAGINATION_CLASS': 'recipes.pagination.CustomPagination',
 }
 
 DJOSER = {
